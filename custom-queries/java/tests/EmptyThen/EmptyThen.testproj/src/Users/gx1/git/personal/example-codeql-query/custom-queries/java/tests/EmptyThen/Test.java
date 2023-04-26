a368331d77b1ec3aa47b818e348b0873c6d402b3
giper45 <18548727+giper45@users.noreply.github.com>
2023-04-26 09:00:21 +0200

    fix: added ql in the qlref

---
+++ custom-queries/java/tests/EmptyThen/EmptyThen.testproj/src/Users/gx1/git/personal/example-codeql-query/custom-queries/java/tests/EmptyThen/Test.java
@@ -0,0 +1,16 @@
+class Test {
+    public void problem(String arg) {
+     if (arg.isEmpty())
+     ;
+     {
+         System.out.println("Empty argument");
+     }
+    }
+    
+    public void good(String arg) {
+     if (arg.isEmpty()) {
+         System.out.println("Empty argument");
+     }
+    }
+    }
+    
